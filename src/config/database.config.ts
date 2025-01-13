5b60ca70f4847d9728e3708bedc3cbf7a6070c4a
houqiang.xie <1624539850@qq.com>
2025-01-13 09:56:03 +0800

    增加查询缓存

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -1,3 +1,11 @@
+/*
+ * @Descripttion: 
+ * @version: 
+ * @Author: houqiangxie
+ * @Date: 2024-12-30 11:17:50
+ * @LastEditors: houqiangxie
+ * @LastEditTime: 2025-01-13 09:54:46
+ */
 // src/config/database.config.ts
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { redisStore } from 'cache-manager-redis-store';
@@ -13,6 +21,14 @@ export const databaseConfig: TypeOrmModuleOptions = {
     synchronize: process.env.NODE_ENV !== 'production',
     logging: process.env.NODE_ENV !== 'production',
     // ssl: process.env.DB_SSL === 'true',
+    cache: {
+        duration: 1000,
+        type: 'redis',
+        options: {
+            host: 'localhost',
+            port: 6379,
+        },
+    }
 };
 
 export const redisConfig = {
